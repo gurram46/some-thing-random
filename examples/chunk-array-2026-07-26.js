8a434dd43bdb4e4042e08574518f4e5c442498ce
gurram46 <81631512+gurram46@users.noreply.github.com>
2026-07-26 21:08:48 +0530

    chore: add chunk array example

---
+++ examples/chunk-array-2026-07-26.js
@@ -0,0 +1,11 @@
+function chunkArray(items, size) {
+  const chunks = [];
+
+  for (let index = 0; index < items.length; index += size) {
+    chunks.push(items.slice(index, index + size));
+  }
+
+  return chunks;
+}
+
+console.log(chunkArray([1, 2, 3, 4, 5], 2));
